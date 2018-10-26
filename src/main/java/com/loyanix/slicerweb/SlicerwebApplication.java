e9f1d1b4fc3339a30ae36259dfc8a71f5891ddf0
Maksim Loyan <loyanmaksim@gmail.com>
2018-10-26 20:36:49 +0300

    Initial commit

---
+++ src/main/java/com/loyanix/slicerweb/SlicerwebApplication.java
@@ -0,0 +1,12 @@
+package com.loyanix.slicerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlicerwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlicerwebApplication.class, args);
+    }
+}
